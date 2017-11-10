f5aaa137645fb400d504cf5c1c7787b60f44efee
Sarah Packowski <spackows@ca.ibm.com>
2017-11-09 20:06:21 -0500

    Update 13_create-r-script.js

--- sample-code/13_create-r-script.js
+++ sample-code/13_create-r-script.js
@@ -1,3 +1,5 @@
+// demo video: https://raw.githubusercontent.com/spackows/CASCON-2017_Analyzing_chat/master/demo-videos/call-13_cluster-normalized-entities.mp4
+//
 
 var g_readFiles = require( './helper_readFiles.js' );
 
