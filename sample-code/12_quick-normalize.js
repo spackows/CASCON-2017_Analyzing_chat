28cf1ae88d4746bd24a3559efbb9a1a96b4c424e
Sarah Packowski <spackows@ca.ibm.com>
2017-11-09 20:05:29 -0500

    Update 12_quick-normalize.js

--- sample-code/12_quick-normalize.js
+++ sample-code/12_quick-normalize.js
@@ -1,3 +1,5 @@
+// demo video: https://raw.githubusercontent.com/spackows/CASCON-2017_Analyzing_chat/master/demo-videos/call-12_normalize.mp4
+//
 
 var g_readFiles = require( './helper_readFiles.js' );
 
