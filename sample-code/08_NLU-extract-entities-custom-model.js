3082d95f93da26cbfde023f8c34743e07990694c
Sarah Packowski <spackows@ca.ibm.com>
2017-11-09 20:03:02 -0500

    Update 08_NLU-extract-entities-custom-model.js

--- sample-code/08_NLU-extract-entities-custom-model.js
+++ sample-code/08_NLU-extract-entities-custom-model.js
@@ -1,3 +1,5 @@
+// demo video: https://raw.githubusercontent.com/spackows/CASCON-2017_Analyzing_chat/master/demo-videos/call-08_extract-entities-custom-model.mp4
+//
 
 var g_nluV1 = require( 'watson-developer-cloud/natural-language-understanding/v1.js' );
 var g_nlu   = new g_nluV1( { username: '', // <- Get this from "Service credentials" section of your Watson NLU service in IBM Cloud dashboard
